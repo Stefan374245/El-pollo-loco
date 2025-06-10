f7adf2445e88c64c750aa7272af79479911df325
Stefan374245 <shelldobler@gmx.de>
2025-06-10 10:25:40 +0200

    implemented level object

--- models/world.class.js
+++ models/world.class.js
@@ -1,42 +1,11 @@
 class World {
   character = new Character();
 
-  enemies = [new Enemy(), new Enemy(), new Enemy()];
-
-  clouds = [new Clouds()];
-
-  backgroundObject = [
-    new BackgroundObject("img/5_background/layers/air.png", -719),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
-
-    new BackgroundObject("img/5_background/layers/air.png", 0),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-
-    new BackgroundObject("img/5_background/layers/air.png", 719),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 ),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+  enemies = level1.enemies;
+  clouds = level1.clouds;
+  backgroundObject = level1.backgroundObject;
 
 
-    new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
-    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
-    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
-
-    new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
-    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
-    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
-    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
-  ];
   canvas;
   bottle = new Bottle();
   statusBar = new statusBar();
